dbc52ff6d18a785ecf18d496f43ccb08af4a49aa
Шаврова Марина Петровна <maryna.shavrova@privatbank.ua>
2022-07-29 23:18:56 +0300

    New CSS style for mobile

--- main/main.js
+++ main/main.js
@@ -1,39 +1,37 @@
-const NAV = document.querySelector('.nav');
-window.addEventListener('scroll', fixNav)
+const NAV = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
 
-function fixNav(){
-    if(window.scrollY > NAV.offsetHeight +150){
-    NAV.classList.add('active');
-    }else{
-      NAV.classList.remove('active');  
-    }
+function fixNav() {
+  if (window.scrollY > NAV.offsetHeight + 150) {
+    NAV.classList.add("active");
+  } else {
+    NAV.classList.remove("active");
+  }
 }
 
+const PANELS = document.querySelectorAll(".panel");
 
-const PANELS = document.querySelectorAll('.panel');
+PANELS.forEach((PANEL) => {
+  PANEL.addEventListener("click", () => {
+    RemoveActiveClasses();
+    PANEL.classList.add("active");
+  });
+});
 
-PANELS.forEach((PANEL)=>{
-    PANEL.addEventListener('click', () =>{
-        RemoveActiveClasses();
-        PANEL.classList.add('active');
-    })
-})
-
-function RemoveActiveClasses(){
-    PANELS.forEach((PANEL)=>{
-        PANEL.classList.remove('active');
-    })
+function RemoveActiveClasses() {
+  PANELS.forEach((PANEL) => {
+    PANEL.classList.remove("active");
+  });
 }
 
-const OPEN_BTN = document.querySelector('.open-btn');
-const CLOSE_BTN = document.querySelector('.close-btn');
-const NAV_MOB = document.querySelectorAll('.hamburger');
+const OPEN_BTN = document.querySelector(".open-btn");
+const CLOSE_BTN = document.querySelector(".close-btn");
+const NAV_MOB = document.querySelectorAll(".hamburger");
 
-OPEN_BTN.addEventListener('click', ()=>{
-    NAV_MOB.forEach(nav_el => nav_el.classList.add('visible'))
-})
+OPEN_BTN.addEventListener("click", () => {
+  NAV_MOB.forEach((nav_el) => nav_el.classList.add("visible"));
+});
 
-CLOSE_BTN.addEventListener('click', ()=>{
-    NAV_MOB.forEach(nav_el => nav_el.classList.remove('visible'))
-})
-
+CLOSE_BTN.addEventListener("click", () => {
+  NAV_MOB.forEach((nav_el) => nav_el.classList.remove("visible"));
+});
